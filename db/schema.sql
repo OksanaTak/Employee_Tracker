ef864a8ea8377ed86341fd932740007702a730dc
Oksana Tatsyak <oksana.tatsyak@gmail.com>
2023-08-08 15:57:49 -0700

    Executed schema and seeds files and corrected errors

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
 DROP DATABASE IF EXISTS employee_tracker_db;
-CREATE DATABASE mployee_tracker_db;
+CREATE DATABASE employee_tracker_db;
 
-USE mployee_tracker_db;
+USE employee_tracker_db;
 
 CREATE TABLE Department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -13,15 +13,15 @@ CREATE TABLE Role (
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
-    FOREIGN KEY (department_id),
-    REFERENCES department(id),
-    ON DELETE SET NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
 );
 
 create table employee (
-  id int not null auto_increment,
+  id int not null auto_increment PRIMARY KEY,
   first_name VARCHAR(30),
   last_name varchar(30),
   role_id INT REFERENCES role(id), 
   manager_id INT REFERENCES employee(id) ON DELETE SET NULL
-)
+);
--- db/seed.sql
+++ db/seed.sql
@@ -8,10 +8,10 @@ INSERT INTO ROLE(title, salary, department_id) VALUES ("Manager of Sales", 15000
 ("Full Stack Developer",250000, 1),("Sales Person",120000, 2), ("Chief Financial Officer", 280000,2);
 
 INSERT INTO EMPLOYEE (first_name, last_name, role_id, manager_id) VALUES
-("John","Osborne",1,NULL),
-("Jeff", "Houke", 2,NULL)
-("Michael", "Fischman", 3, NULL)
-("Rajshree", "Patel", 4,2)
-("Philip", "Fraser", 5, 1)
+("John","Osborne",1, NULL),
+("Jeff", "Houke", 2, NULL),
+("Michael", "Fischman", 3, NULL),
+("Rajshree", "Patel", 4, 2),
+("Philip", "Fraser", 5, 1),
 ("Devan", "Karra", 6, 3);
 
