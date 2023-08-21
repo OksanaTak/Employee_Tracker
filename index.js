6973637afb62052a7291c77360f8374c855fa517
Oksana Tatsyak <oksana.tatsyak@gmail.com>
2023-08-20 21:53:22 -0700

    Added Add Employee functionality

--- index.js
+++ index.js
@@ -82,9 +82,65 @@ function viewEmployees () {
 }
 function viewRoles () {
     console.log('Display all roles')
-    db.query('select * from role;', function (err, result) {
+   db.query('select * from role;', function (err, result) {
       if (err) throw err
       console.table(result)
       init()
-    })
+    }) 
+  }
+
+  function addEmployee (){
+    inquirer.prompt([
+      {
+        type:"input", 
+        massage: "Enter Employee's First Name", 
+        name: "firstName"
+
+      },
+      {
+        type:"input", 
+        massage: "Enter Employee's Last Name", 
+        name: "lastName"
+
+      },
+      {
+        type:"list", 
+        massage: "Enter Employee's Role ", 
+        name: "roleID",
+        choices:[
+          {name:"Manager of Sales",value:1},
+          {name:"Manager of IT ",value:2},
+          {name:"Manager of HR",value:3},
+          {name:"Full Stack Developer ",value:4},
+          {name:"Sales Person",value:5},
+          {name:"Chief Financial Officer",value:6},
+        ]
+
+      },
+      {
+        type:"list", 
+        massage: "Enter Manager", 
+        name: "managerID",
+        choices: [
+          {name:"John Osborne", value:1},
+          {name:"Jeff Houke", value:2},
+          {name:"Michael Fischman", value:3},
+          {name:"Not Applicable", value: null},
+
+
+        ]
+        
+
+      },
+    ])
+    .then(response => {
+      db.query('INSERT INTO EMPLOYEE (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?);',
+      [response.firstName, response.lastName, response.roleID, response.managerID]
+      , function (err, result) {
+        if (err) throw err
+        console.table(result)
+        init()
+      })
+    });
+
   }
